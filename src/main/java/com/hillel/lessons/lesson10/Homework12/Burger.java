55fd1069d5fbf106b572234d184194461083c84b
Nikita Yakoviuk <jakovyukjakov2012@gmail.com>
2023-01-24 10:35:53 +0200

    Fix homework12

--- src/main/java/com/hillel/lessons/lesson10/Homework12/Burger.java
+++ src/main/java/com/hillel/lessons/lesson10/Homework12/Burger.java
@@ -8,21 +8,21 @@ public class Burger {
     String greens;
     String mayonnaise;
 
-    public Burger(String bun, String meat, String cheese, String greens, String mayonnaise) {
+    public Burger(String name, String bun, String meat, String cheese, String greens, String mayonnaise) {
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.greens = greens;
         this.mayonnaise = mayonnaise;
-        System.out.println("Бургер звичайний складається з" + this.bun + "," + this.meat + "," + this.cheese + "," + this.greens + " і" + this.mayonnaise);
+        System.out.println("Бургер " + name + " складається з" + this.bun + "," + this.meat + "," + this.cheese + "," + this.greens + " і" + this.mayonnaise);
     }
 
-    public Burger(String bun, String meat, String cheese, String greens) {
+    public Burger(String name, String bun, String meat, String cheese, String greens) {
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.greens = greens;
-        System.out.println("Бургер дієтичний складається з" + this.bun + "," + this.meat + "," + this.cheese + " і" + this.greens);
+        System.out.println("Бургер " + name + " складається з" + this.bun + "," + this.meat + "," + this.cheese + " і" + this.greens);
     }
 
     public Burger(String bun, String cheese, String greens, String mayonnaise) {
--- src/main/java/com/hillel/lessons/lesson10/Homework12/BurgerMain.java
+++ src/main/java/com/hillel/lessons/lesson10/Homework12/BurgerMain.java
@@ -5,8 +5,8 @@ import com.hillel.lessons.lesson10.Homework12.Burger;
 public class BurgerMain {
     public static void main(String[] args) {
 
-        Burger burger = new Burger(" булочки", " м'яса", " сиру", " зелені", " майонезу");
-        Burger dietBurger = new Burger(" булочки", " м'яса", " сиру", " зелені");
+        Burger burger = new Burger("звичайний", " булочки", " м'яса", " сиру", " зелені", " майонезу");
+        Burger dietBurger = new Burger("дієтичний"," булочки", " м'яса", " сиру", " зелені");
         Burger veganBurger = new Burger(" булочки", " сиру", " зелені", " майонезу");
 
 
